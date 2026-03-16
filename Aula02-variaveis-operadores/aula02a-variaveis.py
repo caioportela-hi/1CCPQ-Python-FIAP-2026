1bfaad1b73433211c9d6518147b54e216e66e424
caioportela-hi <caioportdev@gmail.com>
2026-03-16 20:48:28 -0300

    teste

---
+++ Aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,30 @@
+print('olá, mundo')
+
+print(7 + 4)
+print('7 + 4')
+print('7' + '4') #concatenando strings
+
+# Isso é um comentário de uma linha
+
+'''
+    Comentários
+    de multiplas 
+    linhas
+'''
+
+# Variáveis
+
+nome = 'Caio'
+idade = 19
+peso = 62
+
+print(nome, idade, peso)
+print(f'Olá, {nome}!')
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = input('Digite o seu nome: ')
+idade = input('Digite a sua idade: ')
+peso = input('Digite o seu peso: ')
+
+print(nome, idade, peso)
