9ee8bc738d08cda7a6ea56118e0f31548b3c2f97
caioportela-hi <caioportdev@gmail.com>
2026-04-06 21:04:52 -0300

    aula 3 + exercícios

---
+++ aula03-modulos/aula03-ex.py
@@ -0,0 +1,52 @@
+# EXERCÍCIO 1
+#
+# from playsound3 import playsound
+#
+# playsound("https://www.youtube.com/watch?v=kPa7bsKwL-c&list=RDkPa7bsKwL-c&start_radio=1")
+
+# EXECÍCIO 2
+#
+# num = int(input("Digite um número: "))
+#
+# if num % 2 == 0:
+#     print(f"{num} é par!!")
+# else:
+#     print(f"{num} é ímpar!!")
+
+# EXECÍCIO 3
+#
+# numA = int(input("Digite um número inteiro qualquer: "))
+# numB = int(input("Digite um número inteiro qualquer: "))
+#
+# if numA > numB:
+#     print(f"{numA} é maior que {numB}!!")
+# elif numB > numA:
+#     print(f"{numB} é maior que {numA}!!")
+# else:
+#     print("Os número são iguais bobão!!")
+
+# EXERCÍCIO 4
+
+# n1 = float(input("   DIGITE NOTA DO 1° TRIMESTE: ").replace(',', '.'))
+# n2 = float(input(" \n  DIGITE NOTA DO 2° TRIMESTE: ").replace(',', '.'))
+# n3 = float(input(" \n  DIGITE NOTA DO 3° TRIMESTE: ").replace(',', '.'))
+# n4 = float(input(" \n  DIGITE NOTA DO 3° TRIMESTE: ").replace(',', '.'))
+#
+#
+# vnotas = n1 + n2 + n3 + n4
+#
+# vtotal = vnotas / 4
+#
+# # PRINTS
+# print(f" \n  SUA NOTA FINAL É: {vtotal:.1f}")
+#
+# if vtotal >= 7:
+#     print("\n  VOCÊ FOI APROVADO, PARABÉNS!")
+#
+# elif vtotal < 7 and (vtotal > 5):
+#     print(" \n  VOCÊ ESTÁ DE EXAME, ESTUDE!")
+#
+# if vtotal < 5:
+#     print(' \n  VOCÊ FOI REPROVADO!')
+
+
